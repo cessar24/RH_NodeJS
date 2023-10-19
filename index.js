367d8566078894f6ec8ba3880e8652e9fc96009e
cesar sanchez <cessar123356@gmail.com>
2023-10-18 18:22:25 -0600

    interfaz login

---
+++ index.js
@@ -0,0 +1,29 @@
+//dependencies
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+//routes
+const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
+//middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
+const cors = require('./middleware/cors');
+
+app.use(cors);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", index);
+
+app.use("/user", user);
+app.use(auth);
+app.use("/pokemon", pokemon);
+
+app.use(notFound);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("server is runing...");
+});
